eaec1e452d2cb441fdf4c5306e30c7f86ec115d4
Rianna Cantarelli <rianna.aeon@gmail.com>
2015-07-31 22:16:08 -0300

    Mod Test

--- Interface.js
+++ Interface.js
@@ -14,7 +14,7 @@
 
 
 
-// --------------------
+// ---------------------
 // Caso não exista, inicia objeto CodeCraft
 var CodeCraft = (CodeCraft || function () { });
 if(typeof(CodeCraft) === 'function') { CodeCraft = new CodeCraft(); };
